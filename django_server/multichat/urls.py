9d706d96058a8b3d16a1f2b4099d6df82568b82b
Toms <toms63@yahoo.com>
2018-06-20 22:08:46 +0200

    fixed the real time mouse point issue

--- django_server/multichat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-from django.contrib.auth.views import login, logout
-from chat.views import index
-
-
-urlpatterns = [
-    path('', index),
-]
